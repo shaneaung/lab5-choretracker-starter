74266b7f4be90a3044fce9485ee178b6d9325542
user name <shane.t.aung@gmail.com>
2021-02-04 13:38:41 -0600

    added validation matcher tests to child_test.rb

--- test/models/child_test.rb
+++ test/models/child_test.rb
@@ -6,6 +6,8 @@ class ChildTest < ActiveSupport::TestCase
   should have_many(:tasks).through(:chores)
   
   # TODO: Validation macros
+  should validate_presence_of(:first_name)
+  should validate_presence_of(:last_name)
 
   # TODO: Context testing
 
